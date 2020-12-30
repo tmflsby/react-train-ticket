0ad782640545f38ba3973afd9c3c97c3bfab28f6
tmflsby <823246840@qq.com>
2020-12-30 10:51:25 +0800

    TodoList

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import TodoList from "./TodoList";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <TodoList/>
   </React.StrictMode>,
   document.getElementById('root')
 );
